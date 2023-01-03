81c5302c743246f9d2f339cbb8e032076dc63b4b
Corbin Crutchley <corbin.crutchley@nwyc.com>
2023-01-03 10:56:32 -0800

    chore: demo poc of shared elements

--- apps/vite-react-app/src/App.tsx
+++ apps/vite-react-app/src/App.tsx
@@ -1,14 +1,45 @@
-import { Text } from 'react-native';
-import styled from 'styled-components/native';
+import { useState } from "react";
+import type { PropsWithChildren } from "react";
+import { Text, View } from "react-native";
+import styled from "styled-components/native";
+
+import { RoundedTextInput, ThemeProvider } from "@crutchcorn/shared-elements";
+
+const RTextInput = () => {
+  const [value, setValue] = useState("");
+  return (
+    <RoundedTextInput
+      placeholder="Hello, friends"
+      value={value}
+      onValueChange={setValue}
+    />
+  );
+};
 
 const RedText = styled(Text)`
-  color: red;
+  background: red;
+`;
+
+const TextInputContainer = styled(View)`
+    display: flex;
+    flex-direction: row;
 `
 
-function App() {
+export const Test = ({ children }: PropsWithChildren<any>) => {
   return (
-    <RedText>Testing</RedText>
-  )
+    <ThemeProvider>
+      <View>
+        <RedText>{children}</RedText>
+      </View>
+      <TextInputContainer>
+        <RTextInput />
+      </TextInputContainer>
+    </ThemeProvider>
+  );
+};
+
+const App = () => {
+  return <Test>Testing 123</Test>
 }
 
-export default App
+export default App;
