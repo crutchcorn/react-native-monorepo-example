1d57075ac57d35aa094685693e1fe43ff2316c46
Corbin Crutchley <corbin.crutchley@nwyc.com>
2023-01-03 10:54:12 -0800

    chore: initial work on supporting Styled Components

--- apps/vite-react-app/src/App.tsx
+++ apps/vite-react-app/src/App.tsx
@@ -1,13 +1,13 @@
 import { Text } from 'react-native';
 import styled from 'styled-components/native';
 
-// const RedText = styled(Text)`
-//   color: red;
-// `
+const RedText = styled(Text)`
+  color: red;
+`
 
 function App() {
   return (
-    <Text>Testing</Text>
+    <RedText>Testing</RedText>
   )
 }
 
--- apps/vite-react-app/vite.config.ts
+++ apps/vite-react-app/vite.config.ts
@@ -1,43 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
+import { viteCommonjs, esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
 import * as path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCommonjs()  ],
+  plugins: [react(), viteCommonjs()],
   optimizeDeps: {
     include: [
-      '@crutchcorn/astro-react-native-styled-components/client.js',
       'react',
       'react/jsx-runtime',
       'react/jsx-dev-runtime',
-      'react-dom'
-    ]
+      'react-dom',
+      "styled-components",
+      "styled-components/native",
+    ],
   },
   build: {
     commonjsOptions: {
-      transformMixedEsModules: true,
+      transformMixedEsModules: true
     },
   },
   resolve: {
     dedupe: ['react', 'react-dom', 'react-native-web', 'styled-components', 'styled-components/native'],
     alias: {
       'react-native': 'react-native-web',
-      // // styled-components/native does not like me :(
-      // "@emotion/stylis": path.resolve(__dirname, "stylis-patch.js"),
-      // "css-to-react-native": path.resolve(__dirname, "css-to-react-native-patch.js")
+      "postcss-safe-parser": path.resolve(process.cwd(), "config", "noop.js"),
     },
   },
   ssr: {
     external: [
       'react-dom/server',
-      'react-dom/client'
+      'react-dom/client',
     ],
     noExternal: [
       'styled-components',
       'styled-components/native',
-      '@babel/runtime',
     ],
   },
 })
