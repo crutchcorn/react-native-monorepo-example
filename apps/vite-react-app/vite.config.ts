cf70e5e5c254623be0b5e52716f7a770cb8bf2aa
Corbin Crutchley <corbin.crutchley@nwyc.com>
2023-01-03 09:40:32 -0800

    chore: initial RNW support

--- apps/vite-react-app/vite.config.ts
+++ apps/vite-react-app/vite.config.ts
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCommonjs()  ],
+  optimizeDeps: {
+    include: [
+      '@crutchcorn/astro-react-native-styled-components/client.js',
+      'react',
+      'react/jsx-runtime',
+      'react/jsx-dev-runtime',
+      'react-dom'
+    ]
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  resolve: {
+    dedupe: ['react', 'react-dom', 'react-native-web', 'styled-components', 'styled-components/native'],
+    alias: {
+      'react-native': 'react-native-web',
+      // // styled-components/native does not like me :(
+      // "@emotion/stylis": path.resolve(__dirname, "stylis-patch.js"),
+      // "css-to-react-native": path.resolve(__dirname, "css-to-react-native-patch.js")
+    },
+  },
+  ssr: {
+    external: [
+      'react-dom/server',
+      'react-dom/client'
+    ],
+    noExternal: [
+      'styled-components',
+      'styled-components/native',
+      '@babel/runtime',
+    ],
+  },
 })
