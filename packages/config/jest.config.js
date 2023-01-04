4193131bc170f17d7a9058166654312697121c9f
Corbin Crutchley <git@crutchcorn.dev>
2023-01-04 01:44:22 -0800

    chore: fix lint

--- packages/config/jest.config.js
+++ packages/config/jest.config.js
@@ -20,9 +20,12 @@ module.exports = {
     "^react-native$": "<rootDir>/node_modules/react-native",
     "^react$": "<rootDir>/node_modules/react",
     "^@tanstack/react-query$": "<rootDir>/node_modules/@tanstack/react-query",
-    "^styled-components/native$": "<rootDir>/node_modules/styled-components/native",
-    "^react-native-vector-icons/MaterialIcons$": "<rootDir>/node_modules/react-native-vector-icons/MaterialIcons",
-    "^react-native-vector-icons/MaterialCommunityIcons$": "<rootDir>/node_modules/react-native-vector-icons/MaterialCommunityIcons"
+    "^styled-components/native$":
+      "<rootDir>/node_modules/styled-components/native",
+    "^react-native-vector-icons/MaterialIcons$":
+      "<rootDir>/node_modules/react-native-vector-icons/MaterialIcons",
+    "^react-native-vector-icons/MaterialCommunityIcons$":
+      "<rootDir>/node_modules/react-native-vector-icons/MaterialCommunityIcons",
   },
   setupFiles: [path.resolve(__dirname, "./jest/setup-files.js")],
   setupFilesAfterEnv: [
