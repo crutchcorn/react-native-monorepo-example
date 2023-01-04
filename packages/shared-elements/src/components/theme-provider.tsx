9bdbeaf5c8fff6a8f2d89f78c25b7233284bd7b4
Corbin Crutchley <git@crutchcorn.dev>
2023-01-04 02:02:47 -0800

    chore: support theme provider for both versions of styled components

--- packages/shared-elements/src/components/theme-provider.tsx
+++ packages/shared-elements/src/components/theme-provider.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren, useMemo } from "react";
 import { useColorScheme } from "react-native";
-import { ThemeProvider as StyledThemeProvider } from "styled-components/native";
+import { ThemeProvider as NativeStyledThemeProvider } from "styled-components/native";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
 import { dark, light } from "../constants";
 import { useDimensions } from "../hooks";
 
@@ -13,6 +14,10 @@ export const ThemeProvider = ({ children }: PropsWithChildren) => {
     [theme, dimensions]
   );
   return (
-    <StyledThemeProvider theme={styledTheme}>{children}</StyledThemeProvider>
+    <StyledThemeProvider theme={styledTheme}>
+      <NativeStyledThemeProvider theme={styledTheme}>
+        {children}
+      </NativeStyledThemeProvider>
+    </StyledThemeProvider>
   );
 };
