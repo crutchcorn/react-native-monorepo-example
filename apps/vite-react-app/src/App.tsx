25e2b58182762d45fce8959100122d67785dc680
Corbin Crutchley <git@crutchcorn.dev>
2023-01-03 18:23:17 -0800

    feat!: add initial routing

--- apps/vite-react-app/src/App.tsx
+++ apps/vite-react-app/src/App.tsx
@@ -1,13 +1,22 @@
 import { ThemeProvider } from "@crutchcorn/shared-elements";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MessagesView } from "./views/messages.view";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 const queryClient = new QueryClient();
 
 export const App = () => {
   return (
     <ThemeProvider>
       <QueryClientProvider client={queryClient}>
-        <MessagesView/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Outlet />}>
+              <Route index element={<MessagesView />} />
+              <Route path="other" element={<p>Testing</p>} />
+              <Route path="*" element={<p>There is nothing here 404</p>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </QueryClientProvider>
     </ThemeProvider>
   );
