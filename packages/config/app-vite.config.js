1ed20156abc06fa3188267426ca79315537f7fa2
Corbin Crutchley <git@crutchcorn.dev>
2023-01-04 00:53:57 -0800

    chore: fix build on Vite app

--- packages/config/app-vite.config.js
+++ packages/config/app-vite.config.js
@@ -2,12 +2,6 @@ const path = require("path");
 
 module.exports = {
   optimizeDeps: {
-    // Needed for react-native-icons
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
     include: [
       'react',
       'react/jsx-runtime',
@@ -24,21 +18,23 @@ module.exports = {
     },
   },
   resolve: {
-    dedupe: ['react', 'react-dom', 'react-native-web', 'styled-components', 'styled-components/native'],
+    dedupe: [
+      'react',
+      'react-dom',
+      'react-native-web',
+      'styled-components',
+      'styled-components/native',
+      "react-native-vector-icons/MaterialIcons",
+      "react-native-vector-icons/MaterialCommunityIcons",
+      "react-native-vector-icons",
+      "@tanstack/react-query",
+    ],
     alias: {
       'react-native': 'react-native-web',
+      "react-native-vector-icons/MaterialIcons": "react-native-vector-icons/dist/MaterialIcons",
+      "react-native-vector-icons/MaterialCommunityIcons": "react-native-vector-icons/dist/MaterialCommunityIcons",
       // Needed for styled-components/native
       "postcss-safe-parser": path.resolve(__dirname, "vite", "noop.js"),
     },
-  },
-  ssr: {
-    external: [
-      'react-dom/server',
-      'react-dom/client',
-    ],
-    noExternal: [
-      'styled-components',
-      'styled-components/native',
-    ],
-  },
+  }
 }
