8662161d04cc540c29d58c32695cbc6c0759a0d9
Corbin Crutchley <git@crutchcorn.dev>
2023-08-02 11:12:09 -0700

    chore: fix storybook

--- packages/shared-elements/.storybook/main.ts
+++ packages/shared-elements/.storybook/main.ts
@@ -1,10 +1,15 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import { mergeConfig } from 'vite';
-import appViteConfig from '@crutchcorn/config/app-vite.config';
+import {InlineConfig, mergeConfig} from 'vite';
+import _appViteConfig from '@crutchcorn/config/app-vite.config';
+import {removeMobileCodePlugin} from "../vite/remove-mobile-code-plugin";
+
+const appViteConfig: InlineConfig = _appViteConfig as never;
 
 const config: StorybookConfig = {
   async viteFinal(config) {
-    return mergeConfig(config, (appViteConfig as never));
+    return mergeConfig(config, mergeConfig(appViteConfig, {plugins: [removeMobileCodePlugin()], define: {'process.env': {
+      IS_WEB: true
+        }}}));
   },
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", {
--- packages/shared-elements/.storybook/preview.js
+++ packages/shared-elements/.storybook/preview.js
@@ -1,3 +1,7 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
